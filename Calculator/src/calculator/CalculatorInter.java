4101f8693589dd9994c6136b77b92c1dfa7cead4
Trần Phi Anh <59632363+anhtpde140084@users.noreply.github.com>
2020-09-19 14:30:55 +0700

    Add files via upload

---
+++ Calculator/src/calculator/CalculatorInter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author tranp
+ */
+public interface CalculatorInter {
+
+    void sub(double a, double b);
+
+    void subtract(double a, double b);
+
+    void multiply(double a, double b);
+
+    void divide(double a, double b);
+}
